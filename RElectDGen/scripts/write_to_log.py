27d77d1053a756befb16df69b87d09234be30323
emilannevelink <emilannevelink@hotmail.com>
2022-01-05 14:00:43 -0500

    first commit

---
+++ RElectDGen/scripts/write_to_log.py
@@ -0,0 +1,51 @@
+import argparse, subprocess
+from ase.parallel import world
+import operator
+import numpy as np
+import yaml
+import os
+import json
+import pandas as pd
+
+def parse_command_line(argsin):
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--config_file', dest='config',
+                        help='active_learning configuration file', type=str)
+    args = parser.parse_args(argsin)
+
+    with open(args.config,'r') as fl:
+        config = yaml.load(fl,yaml.FullLoader)
+
+    return config
+
+
+def main(args = None):
+
+    config = parse_command_line(args)
+
+    #load tmp
+    tmp_filename = os.path.join(config.get('directory'),config.get('run_dir'),config.get('tmp_file','tmp.json'))
+    with open(tmp_filename,'r') as fl:
+        tmp_dict = json.load(fl)
+
+    new_line = pd.DataFrame(tmp_dict,index=[0])
+    
+    #create dataframe
+    log_filename = os.path.join(config.get('directory'),config.get('log_filename'))
+    if os.path.isfile(log_filename):
+        dataframe = pd.read_csv(log_filename)
+
+        #write line to dataframe
+        dataframe = dataframe.append(new_line,ignore_index=True) 
+    else:
+        dataframe = new_line
+
+    #save new dataframe
+    dataframe.to_csv(log_filename,index=False)
+
+    os.remove(tmp_filename)
+
+if __name__ == "__main__":
+    main()
+                
+
---
+++ RElectDGen/utils/logging.py
@@ -0,0 +1,47 @@
+import os
+import json
+import pandas as pd
+import numpy as np
+from .save import get_results_dir
+
+def write_to_tmp_dict(filename,dict):
+    
+    if os.path.isfile(filename):
+        with open(filename,'r') as fl:
+            old_dict = json.load(fl)
+    else:
+        old_dict = {}
+
+    new_dict = {**old_dict,**dict}
+
+    with open(filename,'w') as fl:
+        json.dump(new_dict,fl)
+
+
+def get_mae_from_results():
+
+    results_dir = get_results_dir()
+
+    results_metrics = os.path.join(results_dir,'metrics_epoch.csv')
+    data = pd.read_csv(results_metrics)
+
+    best_ind = np.argmin(data[' validation_loss'])
+
+    mae_dict = {
+        'training_f_mae': data[' training_all_f_mae'][best_ind],
+        'training_e_mae': data[' training_e_mae'][best_ind],
+        'validation_f_mae': data[' validation_all_f_mae'][best_ind],
+        'validation_e_mae': data[' validation_e_mae'][best_ind],
+    }
+
+    return mae_dict
+
+def UQ_params_to_dict(params, prefix: str = ''):
+    if not prefix.endswith('_'):
+        prefix += '_'
+    out_dict = {}
+    for key in params:
+        out_dict[prefix+key+'_base'] = float(params[key][0])
+        out_dict[prefix+key+'_linear'] = float(params[key][1])
+    
+    return out_dict
