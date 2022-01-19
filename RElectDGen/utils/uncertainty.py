181d7c097161813d514b755835f818b12a6287c7
emilannevelink <emilannevelink@hotmail.com>
2022-01-19 07:17:46 -0500

    remove single uncertainty

--- RElectDGen/utils/uncertainty.py
+++ RElectDGen/utils/uncertainty.py
@@ -30,12 +30,10 @@ class latent_distance_uncertainty_Nequip():
         #             data[key].to(torch.device('cuda'))
         return data
 
-    def calibrate(self):
+    def calibrate(self,debug=False):
         #latent_size = self.model.final.tp.irreps_in1.dim #monolayer energy
         
         dataset = dataset_from_config(self.config)
-
-        train_force_latent_distances = torch.empty((0,self.latent_size),device=self.device)
 
         train_embeddings = {}
         test_embeddings = {}
@@ -47,42 +45,25 @@ class latent_distance_uncertainty_Nequip():
     
         for data in dataset[self.config.train_idcs]:
             out = self.model(self.transform_data_input(data))
-            
-            train_force_latent_distances = torch.cat([train_force_latent_distances,out['node_features']])
 
             for key in self.config.get('chemical_symbol_to_type'):
                 mask = data['atom_types']==self.config.get('chemical_symbol_to_type')[key]
                 train_embeddings[key] = torch.cat([train_embeddings[key],out['node_features'][mask]])
 
         self.train_embeddings = train_embeddings
-
-        self.train_force_latent_distances = train_force_latent_distances
-
-        test_force_latent_distances = torch.empty((0,self.latent_size),device=self.device)
-        test_force_errors = torch.empty((0,3),device=self.device)
+            
 
         for data in dataset[self.config.val_idcs]:
             out = self.model(self.transform_data_input(data))
-            test_force_latent_distances = torch.cat([test_force_latent_distances,out['node_features']]) 
             
             error = torch.absolute(out['forces'] - data.forces)
-            test_force_errors = torch.cat([test_force_errors,error])
 
             for key in self.config.get('chemical_symbol_to_type'):
                 mask = data['atom_types']==self.config.get('chemical_symbol_to_type')[key]
                 test_embeddings[key] = torch.cat([test_embeddings[key],out['node_features'][mask]])
                 test_errors[key] = torch.cat([test_errors[key],error.mean(dim=1)[mask]])
+        
         self.test_embeddings = test_embeddings
-
-        self.test_force_errors = test_force_errors.mean(dim=1).detach().cpu().numpy()
-        
-        self.val_latent_distances = test_force_latent_distances
-        self.train_val_latent_distances = torch.cat([train_force_latent_distances,test_force_latent_distances])
-
-        self.latent_force_distances = torch.cdist(train_force_latent_distances,test_force_latent_distances,p=2)
-        
-        inds = torch.argmin(self.latent_force_distances,axis=0)
-        self.d_force_test = torch.tensor([self.latent_force_distances[ind,i] for i, ind in enumerate(inds)]).detach().cpu().numpy()
 
         latent_distances = {}
         min_distances = {}
@@ -92,15 +73,15 @@ class latent_distance_uncertainty_Nequip():
             inds = torch.argmin(latent_distances[key],axis=0)
             min_distances[key] = torch.tensor([latent_distances[key][ind,i] for i, ind in enumerate(inds)]).detach().cpu().numpy()
 
-
             params0 = (0.01,0.01)
             res = minimize(optimizeparams,params0,args=(test_errors[key].reshape(-1).detach().cpu().numpy(),min_distances[key]),method='Nelder-Mead')
             print(res,flush=True)
             params[key] = np.abs(res.x)
 
         self.params = params
-        self.min_distances = min_distances
-        self.test_errors = test_errors
+        if debug:  
+            self.min_distances = min_distances
+            self.test_errors = test_errors
 
     def predict(self, data, distances='train_val'):
         out = self.model(self.transform_data_input(data))
@@ -153,19 +134,7 @@ class latent_distance_uncertainty_Nequip():
             return uncertainty, atom_embeddings.reshape(len(traj),-1,atom_embeddings.shape[-1])
 
     def plot_fit(self):
-        fig = plt.figure()
-        ax = plt.gca()
-        ax.scatter(self.d_force_test,self.test_force_errors.reshape(-1),alpha=0.2)
-
-        sigabs = np.abs(self.sigmas)
-        d_fit = np.linspace(0,self.d_force_test.max())
-        error_fit = sigabs[0] + sigabs[1]*d_fit
-        ax.plot(d_fit,error_fit,'r')
-
-        ax.set_title(f'Sigmas: {sigabs[0]}, {sigabs[1]}')
-        ax.set_xlabel('Embedding Distance')
-        ax.set_ylabel('Force Error')
-
+        
         fig = plt.figure()
         ax = plt.gca()
         for key in self.config.get('chemical_symbol_to_type'):
