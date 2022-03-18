2955dbea14febaf3c7ec1ec3429d2e069dde257c
emilannevelink <emilannevelink@hotmail.com>
2022-03-18 08:53:04 -0400

    add equation calculator

---
+++ RElectDGen/calculate/ase_equation.py
@@ -0,0 +1,59 @@
+from typing import Union, Optional, Callable, Dict
+import warnings
+import torch
+import numpy as np
+
+import ase.data
+from ase.calculators.calculator import Calculator, all_changes
+import ase
+
+class FunctionCalculator(Calculator):
+
+    implemented_properties = ["energy", "forces"]
+
+    def __init__(
+        self,
+        function,
+        cutoff = 2,
+        **kwargs
+    ):
+        Calculator.__init__(self, **kwargs)
+        self.results = {}
+        self.function = function
+        self.cutoff = cutoff
+
+    def calculate(self, atoms=None, properties=["energy"], system_changes=all_changes):
+        """
+        Calculate properties.
+
+        :param atoms: ase.Atoms object
+        :param properties: [str], properties to be computed, used by ASE internally
+        :param system_changes: [str], system changes since last calculation, used by ASE internally
+        :return:
+        """
+        # call to base-class to set atoms attribute
+        Calculator.calculate(self, atoms)
+
+        first_index, second_index, shifts = ase.neighborlist.primitive_neighbor_list(
+            "ijS",
+            atoms.pbc,
+            atoms.cell,
+            atoms.positions,
+            cutoff=float(self.cutoff),
+            self_interaction=False,  # we want edges from atom to itself in different periodic images!
+            use_scaled_positions=False,
+        )
+
+
+        positions = torch.tensor(atoms.positions, requires_grad=True)
+        pair_distances = positions[first_index] - positions[second_index] + torch.tensor(np.dot(shifts,atoms.cell))
+        # pair_distances = torch.tensor(D,requires_grad=True)
+        pair_distances = torch.abs(pair_distances)
+        energy = self.function(pair_distances).sum()/2.
+        forces = torch.autograd.grad(energy,positions)[0]
+
+        # store results
+        self.results = {
+            "energy": energy.detach().numpy(),
+            "forces": forces.detach().numpy(),
+        }
--- RElectDGen/calculate/calculator.py
+++ RElectDGen/calculate/calculator.py
@@ -11,54 +11,65 @@ from RElectDGen.utils.save import get_results_dir
 
 def oracle_from_config(config,atoms=None):
 
-    from gpaw import GPAW, PW, FermiDirac, restart
-    from gpaw.eigensolvers import Davidson
-    from gpaw.poisson import PoissonSolver
-    from gpaw.xc.vdw import VDWFunctional
+    calculator_type = config.get('calculator_type', 'gpaw')
+    if calculator_type == 'gpaw':
+        from gpaw import GPAW, PW, FermiDirac, restart
+        from gpaw.eigensolvers import Davidson
+        from gpaw.poisson import PoissonSolver
+        from gpaw.xc.vdw import VDWFunctional
 
-    if 'vdW' in config.get('xc'):
-        xc = VDWFunctional(config.get('xc'))
-    else:
-        xc=config.get('xc')
+        if 'vdW' in config.get('xc'):
+            xc = VDWFunctional(config.get('xc'))
+        else:
+            xc=config.get('xc')
 
-    if atoms is not None:
-        config['cell'] = atoms.cell
-        config['pbc'] = atoms.pbc
-        if sum(atoms.pbc)==0:
-            atoms.center()
-        charge = atoms.get_initial_charges().sum().round()
-    else:
-        charge = 0
-    
-    if world.rank == 0:
-        print('Charge: ', charge)
+        if atoms is not None:
+            config['cell'] = atoms.cell
+            config['pbc'] = atoms.pbc
+            if sum(atoms.pbc)==0:
+                atoms.center()
+            charge = atoms.get_initial_charges().sum().round()
+        else:
+            charge = 0
+        
+        if world.rank == 0:
+            print('Charge: ', charge)
 
-    if config.get('kxl') is not None and config.get('cell') is not None:
-        if len(np.shape(config.get('cell'))) == 2:
-            kpts = np.ceil((config.get('kxl')/np.linalg.norm(config.get('cell'),axis=1))).astype(int)
-        elif len(np.shape(config.get('cell'))) == 1:
-            kpts = np.ceil((config.get('kxl')/np.array(config.get('cell')))).astype(int)
+        if config.get('kxl') is not None and config.get('cell') is not None:
+            if len(np.shape(config.get('cell'))) == 2:
+                kpts = np.ceil((config.get('kxl')/np.linalg.norm(config.get('cell'),axis=1))).astype(int)
+            elif len(np.shape(config.get('cell'))) == 1:
+                kpts = np.ceil((config.get('kxl')/np.array(config.get('cell')))).astype(int)
 
-        kpts = [kpts[i] if bool else 1 for i, bool in enumerate(config.get('pbc'))]
-    else:
-        kpts = config.get('kpoints')
+            kpts = [kpts[i] if bool else 1 for i, bool in enumerate(config.get('pbc'))]
+        else:
+            kpts = config.get('kpoints')
 
-    GPAW_dump_file = os.path.join(config.get('data_directory'),config.get('GPAW_dump_file'))
-    calculator = GPAW(
-                xc=xc,
-                kpts=(kpts),
-                h = config.get('grid_spacing'),
-                occupations={'name': config.get('occupation'), 'width': config.get('occupation_width')},
-                eigensolver=Davidson(config.get('Davidson_steps')),
-                poissonsolver=PoissonSolver(),#eps=config.get('Poisson_tol')),
-                symmetry={'point_group': False},
-                # maxiter=333,
-                txt=GPAW_dump_file)
+        GPAW_dump_file = os.path.join(config.get('data_directory'),config.get('GPAW_dump_file'))
+        calculator = GPAW(
+                    xc=xc,
+                    kpts=(kpts),
+                    h = config.get('grid_spacing'),
+                    occupations={'name': config.get('occupation'), 'width': config.get('occupation_width')},
+                    eigensolver=Davidson(config.get('Davidson_steps')),
+                    poissonsolver=PoissonSolver(),#eps=config.get('Poisson_tol')),
+                    symmetry={'point_group': False},
+                    # maxiter=333,
+                    txt=GPAW_dump_file)
 
-    calculator.set(charge=charge)
+        calculator.set(charge=charge)
 
-    if not config.get('pbc')[-1] and np.sum(config.get('pbc'))==2:
-        calculator.set(poissonsolver={'dipolelayer': 'xy'})
+        if not config.get('pbc')[-1] and np.sum(config.get('pbc'))==2:
+            calculator.set(poissonsolver={'dipolelayer': 'xy'})
+
+    elif calculator_type == 'equation':
+        from .ase_equation import FunctionCalculator
+
+        function = eval(config.get('equation'))
+        calculator = FunctionCalculator(
+            function,
+            config.get('cutoff',2)
+        )
 
     return calculator
 
