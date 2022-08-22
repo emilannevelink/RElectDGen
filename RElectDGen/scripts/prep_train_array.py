0c3fce76e936bace58f64a17d5b6460f12c48a96
emilannevelink <emilannevelink@hotmail.com>
2022-08-22 15:35:30 -0400

    change dtype

--- RElectDGen/scripts/prep_train_array.py
+++ RElectDGen/scripts/prep_train_array.py
@@ -123,7 +123,7 @@ def main(args=None):
         n_val_add = MLP_config_new.get('n_val')
 
         traj = Trajectory(MLP_config_new['dataset_file_name'])
-        all_indices = torch.arange(0,len(traj),1).numpy()
+        all_indices = torch.arange(0,len(traj),1,dtype=int)
         
         ind_select = torch.randperm(len(all_indices))
 
