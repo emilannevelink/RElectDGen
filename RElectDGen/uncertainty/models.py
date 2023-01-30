5eae6f2f9170c3dcab24ece93df450b9d53b923b
emilannevelink <emilannevelink@hotmail.com>
2023-01-30 10:51:28 -0500

    make pred_forces a tensor

--- RElectDGen/uncertainty/models.py
+++ RElectDGen/uncertainty/models.py
@@ -1919,7 +1919,7 @@ class Nequip_ensemble(uncertainty_base):
         for atoms in traj:
             uncertainty.append(self.predict_uncertainty(atoms).detach())
             atom_embeddings.append(self.atom_embedding.detach())
-            self.pred_forces.append(self.atom_forces)
+            self.pred_forces.append(self.atom_forces.detach())
         
         uncertainty = torch.cat(uncertainty).cpu()
         atom_embeddings = torch.cat(atom_embeddings).cpu()
@@ -1934,6 +1934,8 @@ class Nequip_ensemble(uncertainty_base):
             
             return torch.vstack([unc[torch.argmax(unc.sum(dim=1))] for unc in uncertainty_partition]), embeddings
         else:
+            self.pred_forces = torch.cat(self.atom_forces.cpu())
+            self.pred_forces = self.pred_forces.reshape(len(traj),-1,3)
             uncertainty = uncertainty.reshape(len(traj),-1, 2)
             return uncertainty, atom_embeddings.reshape(len(traj),-1,atom_embeddings.shape[-1])
 
