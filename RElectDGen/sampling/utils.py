8fff5c3ce65ba9ae15607a01550929b6a72b496e
emilannevelink <emilannevelink@hotmail.com>
2023-07-19 09:45:48 -0400

    always include complex after one is included

--- RElectDGen/sampling/utils.py
+++ RElectDGen/sampling/utils.py
@@ -228,8 +228,8 @@ def sample_from_ase_db(
 ):
     rows_DFT = [row for row in db.select(f'success=True,md_stable<{max_md_stable}')]
 
-    if db.count('success=True,md_stable=0') == 0:
-        rows_complex = [row for row in db.select(f'initial_structure=True,md_stable=0')]
+    if db.count('success=True,md_stable=0') == 0 or db.count('initial_structure=True,md_stable>0')>0:
+        rows_complex = [row for row in db.select(f'initial_structure=True,md_stable<{max_md_stable}')]
     else:
         rows_complex = []
     rows = rows_DFT + rows_complex
