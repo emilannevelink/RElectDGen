837f4253bbbe88199b43dd441b6db19d10cc5f8a
emilannevelink <emilannevelink@hotmail.com>
2023-08-08 10:48:13 -0400

    add backward compatibility for no start_row_id

--- RElectDGen/statistics/energy_range.py
+++ RElectDGen/statistics/energy_range.py
@@ -1,7 +1,12 @@
 from ase.units import kB
+from ase.parallel import world
 
 def check_energy_range(db,atoms,temperature):
-    row_id = atoms.info['start_row_id']
+    row_id = atoms.info.get('start_row_id')
+    if row_id is None:
+        if world.rank == 0:
+            print('start row id not in atoms.info')
+        return True
     atoms_start = db[row_id].toatoms()
     energy_start = atoms_start.get_potential_energy()
     energy_end = atoms.get_potential_energy()
